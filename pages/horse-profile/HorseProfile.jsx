427caf9510b59b15a48090a6624325bcf2b430f4
mikaer86 <mikaeleriksson123@gmail.com>
2023-03-02 12:05:40 +0100

    commit

--- pages/horse-profile/HorseProfile.jsx
+++ pages/horse-profile/HorseProfile.jsx
@@ -1,31 +1,31 @@
-import { StyleSheet, View, Text } from "react-native";
-import { Heading1, Heading2, Paragraph } from "../../components/TextStyles";
+import { StyleSheet, View, Text } from 'react-native';
+import { Heading1, Heading2, Paragraph } from '../../components/TextStyles';
 
 export default function HorseProfile({ horse }) {
-    return (
-        <View style={layoutHorseProfile.layout}>
-            <Heading1>{horse.name}</Heading1>
-            <View style={layoutHorseProfile.statContainer}>
-                <Heading2>{horse.temperature} C</Heading2>
-                <Heading2>{horse.moisture}%</Heading2>
-            </View>
-            <Paragraph>{horse.blanket_type} Blanket</Paragraph>
-        </View>
-    )
-
-
+  const updateData = [];
+  return (
+    <View style={layoutHorseProfile.layout}>
+      <Heading1>{horse.name}</Heading1>
+      <View style={layoutHorseProfile.statContainer}>
+        <Heading2>{horse.temperature} C</Heading2>
+        <Heading2>{horse.moisture}%</Heading2>
+      </View>
+      <Paragraph>{horse.blanket_type} Blanket</Paragraph>
+    </View>
+  );
 }
 
 const layoutHorseProfile = StyleSheet.create({
-    layout: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    statContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        gap: 50,
-        margin: 30
-    }
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#dcdcdc',
+  },
+  statContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    gap: 50,
+    margin: 30,
+  },
 });
