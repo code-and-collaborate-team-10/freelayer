577f92a3ec28b8852155e4afc719457d442f5a4e
mikaer86 <mikaeleriksson123@gmail.com>
2023-03-09 00:57:28 +0100

    globalStyles

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {horseProfile} from "..pages/HorseProfile.jsx";
+
+import {Text, View } from 'react-native';
+import HorseProfile from './pages/horse-profile/HorseProfile';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HorseProfile/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- pages/horse-profile/HorseProfile.jsx
+++ pages/horse-profile/HorseProfile.jsx
@@ -1,48 +1,26 @@
-import { StyleSheet, View, Text } from 'react-native';
-import {
-  Heading1,
-  Heading2,
-  Paragraph,
-  Button,
-} from '../../components/TextStyles';
+import { View, Button, Image, InputText } from 'react-native';
+import { globalStyles, profileImage } from '../../styles/GlobalStyles';
+import useState from 'react-native';
+import DropdownMenu from 'react-native-dropdown-menu';
 
-export default function HorseProfile({ horse }) {
+export default function HorseProfile() {
+  const [state, setState] = useState(false);
+
+  const dropDownHandler = (state) => {
+    return setState();
+  };
   return (
-    <View style={layoutHorseProfile.layout}>
-      <View style={layoutHorseProfile.navBar}>
-        <Button style={}>Back</Button>
+    <View style={globalStyles.layout}>
+      <View style={globalStyles.navBar}>
+        <Image source={profileImage.horses[1]} />
+        <InputText title='Name' />
+        <InputText title='Age' />
       </View>
-      <Heading1>{horse.name}</Heading1>
-      <View style={layoutHorseProfile.statContainer}>
-        <Heading2>{horse.temperature} C</Heading2>
-        <Heading2>{horse.moisture}%</Heading2>
+      <View style={globalStyles.statContainer}>
+        <DropdownMenu style={globalStyles.dropDown} />
+
+        <Button title='Save'></Button>
       </View>
-      <Paragraph>{horse.blanket_type} Blanket</Paragraph>
     </View>
   );
 }
-
-const layoutHorseProfile = StyleSheet.create({
-  layout: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#dcdcdc',
-  },
-  navBar: {
-    flexDirection: 'row',
-    paddingHorizontal: 100,
-  },
-  navBar: {
-    justifyContent: 'flex-start',
-    backgroundColor: 'grey',
-  },
-  statContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    gap: 50,
-    margin: 30,
-  },
-});
-
-export default horseProfile;
